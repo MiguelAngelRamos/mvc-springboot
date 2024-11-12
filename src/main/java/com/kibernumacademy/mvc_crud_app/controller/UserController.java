626e8bb394228fe2b93e58cb8454ea91b4f8aeb1
Miguel Ramos <return.infox@gmail.com>
2024-11-12 09:56:31 -0300

    todos los metodos del controlador

--- src/main/java/com/kibernumacademy/mvc_crud_app/controller/UserController.java
+++ src/main/java/com/kibernumacademy/mvc_crud_app/controller/UserController.java
@@ -9,6 +9,7 @@ import com.kibernumacademy.mvc_crud_app.model.User;
 import com.kibernumacademy.mvc_crud_app.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -29,14 +30,42 @@ public class UserController {
       return "users/index";
   }
 
+  @GetMapping("/create")
+  public String createForm(Model model) {
+    model.addAttribute("user", new User());
+    return "users/create";
+  }
+
   @PostMapping
   public String createUser(@ModelAttribute("user") User user) {
     userService.createUser(user);
     return "redirect:/users";
   }
-  
+
+  @GetMapping("/edit/{id}") // localhost:8080/users/edit/1
+  public String editForm(@PathVariable Long id, Model model) {
+    model.addAttribute("user", userService.getUserById(id));
+    return "users/edit";
+  }
+
+  @PostMapping("/update/{id}") 
+  public String updateUser(@PathVariable Long id, @ModelAttribute("user") User user) {
+    userService.updateUser(id, user);
+    return "redirect:/users"; // a la pagina principal
+  }
+
+  @GetMapping("/delete/{id}")
+  public String deleteUser(@PathVariable Long id) {
+    userService.deleteUser(id);
+    return "redirect/users";
+  }
+
 
 }
+
+
+
+
 
 // localhost:8080/users (GET)
 // localhost:8080/users (POST)
